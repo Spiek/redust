3960004eb0be02a15d0fcd96c22e2b3634b3d231
Spiek <spiek@ymail.com>
2015-09-13 03:14:20 +0200

    Improving * fix protobuf serialization

--- Redismap.pro
+++ Redismap.pro
@@ -29,5 +29,12 @@ HEADERS += \
 DEFINES += "REDISMAP_INIT_QUERY_BUFFER_CACHE_SIZE=1048576"
 DEFINES += "REDISMAP_MAX_QUERY_BUFFER_CACHE_SIZE=10485760"
 
+# Google protobuffer Test messages
+HEADERS += src/test.pb.h
+SOURCES += src/test.pb.cc
+
 # Features
 DEFINES += "REDISMAP_SUPPORT_PROTOBUF"
+
+# link against protobuf lib (we can't do that in mkspecs because the protobuf lib has to be set as last lib)
+LIBS += -lprotobuf
--- src/redismap.h
+++ src/redismap.h
@@ -15,6 +15,49 @@
     #include <google/protobuf/message.h>
 #endif
 
+template< typename T, typename Enable = void >
+class RedisValue
+{
+    // Don't allow data pointer types
+    // Reason: Redismap don't store any keys or values localy, instead redismap just act as a proxy between the program and a remote redisserver which contains the actual data.
+    //         Pointers would be too dangeours, because:
+    //         * the data would not be touched by the Redismap and so maybe become out of date.
+    //         * redismap is not able to handle the ownership of the pointers, qDeleteAll on the values would not working, because no data do exist localy!
+    // Perfomance Tip:
+    // * instead of using pointers you can define the Redismap with reference Types
+    static_assert(!std::is_pointer<T>::value,
+                  "Pointer types are not allowed in Redismap, because redismap is not storing any data!");
+
+    public:
+        static inline QVariant serialize(T value) {  return QVariant(value); }
+        static inline T unserialize(QByteArray value) {  return QVariant(value).value<T>(); }
+};
+
+template<typename T>
+class RedisValue<T, typename std::enable_if<std::is_base_of<google::protobuf::Message, T>::value && !std::is_pointer<T>::value>::type >
+{
+    public:
+        static inline QString serialize(T value) { return QString::fromStdString(value.SerializeAsString()); }
+        static inline T unserialize(QByteArray value) {
+            T t;
+            t.ParseFromArray(value.data(), value.length());
+            return t;
+        }
+};
+
+template<typename T>
+class RedisValue<T&, typename std::enable_if<std::is_base_of<google::protobuf::Message, T>::value>::type >
+{
+    public:
+        static inline QString serialize(T value) { return QString::fromStdString(value.SerializeAsString()); }
+        static inline T unserialize(QByteArray value) {
+            T t;
+            t.ParseFromArray(value.data(), value.length());
+            return t;
+        }
+};
+
+
 template< typename Key, typename Value >
 class RedisMap
 {
@@ -31,8 +74,8 @@ class RedisMap
             if(!RedisMapConnectionManager::checkRedisConnection(this->redisConnection)) return false;
 
             // simplefy key and value
-            QVariant vkey = this->constructVariant(key);
-            QVariant vValue = this->constructVariant(value);
+            QVariant vkey = RedisValue<Key>::serialize(key);
+            QVariant vValue = RedisValue<Value>::serialize(value);
 
             // if something is wrong with key or value, exit
             if(!vkey.isValid() || !vValue.isValid()) {
@@ -73,26 +116,6 @@ class RedisMap
         }
 
     private:
-        // variant type handlers
-#ifdef REDISMAP_SUPPORT_PROTOBUF
-        // protobuf data serial implementation
-        template< typename T,
-                  typename std::enable_if<std::is_same<google::protobuf::Message, T>::value || std::is_same<google::protobuf::Message*, T>::value>::type* = nullptr>
-        QVariant constructVariant(T value)
-        {
-            google::protobuf::Message *message = 0;
-            if(std::is_pointer<T>::value) message = (google::protobuf::Message*)value;
-            else message = (google::protobuf::Message*)&value;
-            return message ? QString::fromStdString(message->SerializeAsString()) : "";
-        }
-#endif
-        template< typename T >
-        QVariant constructVariant(T value)
-        {
-            return value;
-        }
-
-
         static inline bool checkRedisReturnValue(QAbstractSocket* socket)
         {
             return socket && socket->readAll().left(3) == "+OK";
